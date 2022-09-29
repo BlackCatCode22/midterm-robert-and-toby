b1660c54de5eb4fc012ed754bcd47474f1d68a8f
TobyEns <tens3970@gmail.com>
2022-09-29 11:52:27 -0700

    Can randomly generate a name!

--- ZooAnimals.java
+++ ZooAnimals.java
@@ -1,19 +1,39 @@
 import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.util.Random;
 
 public class ZooAnimals {
     public static void main(String[] args){
         String filePath = new File("").getAbsolutePath();
         filePath = filePath.concat("\\animalNames.txt");
-        System.out.println(filePath);
+        Random rand = new Random();
+        String[] animalNames = new String[0];
         try {
+            String names = "";
             File file = new File(filePath);
             Scanner scannerPhil = new Scanner(file);
-            System.out.println(scannerPhil.nextLine());
+            while (scannerPhil.hasNextLine()){
+                String data = scannerPhil.nextLine();
+                if (data.contains(",")){
+                    names = names.concat(data + ";");
+                }
+            }
+            animalNames = names.split(";");
         } catch (FileNotFoundException e){
             System.out.println("who");
             e.printStackTrace();
         }
+        String[][] allNames = {animalNames[0].split(", "),animalNames[1].split(", "),animalNames[2].split(", "),animalNames[3].split(", ")};
+
+        while (true) {
+            int num = rand.nextInt(4);
+            System.out.println(allNames[num][rand.nextInt(allNames[num].length)]);
+        }
+//        for (String[] test:allNames){
+//            for(String e:test){
+//                System.out.println(e);
+//            }
+//        }
     }
 }
