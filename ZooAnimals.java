3273e69bf312cc5a08ec02b7068e7d7417a92972
rjordan5gs <rjordan1994@gmail.com>
2022-09-29 11:00:10 -0700

    Started working on scanner, currently broken.

--- ZooAnimals.java
+++ ZooAnimals.java
@@ -1,12 +1,19 @@
 import java.io.File;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 public class ZooAnimals {
     public static void main(String[] args){
         String filePath = new File("").getAbsolutePath();
         filePath = filePath.concat("\\animalNames.txt");
         System.out.println(filePath);
-        
-
+        try {
+            File file = new File(filePath);
+            Scanner scannerPhil = new Scanner(file);
+            System.out.println(scannerPhil.nextLine());
+        } catch (FileNotFoundException e){
+            System.out.println("who");
+            e.printStackTrace();
+        }
     }
 }
