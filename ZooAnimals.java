6f0bac3e779773de510d92d308db9b3222f9fd2c
TobyEns <tens3970@gmail.com>
2022-10-13 08:57:56 -0700

    Final Version M1

--- ZooAnimals.java
+++ ZooAnimals.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.util.Random;
@@ -55,6 +57,7 @@ public class ZooAnimals {
             e.printStackTrace();
         }
 
+
         try {
             File dataFile = new File(arrivingDataFilePath);
             unParsedData = new String[lineCount][0];
@@ -70,17 +73,151 @@ public class ZooAnimals {
             e.printStackTrace();
         }
 
+        // Get Zoo Data
+        String zooDataFilepath = new File("").getAbsolutePath();
+        zooDataFilepath = zooDataFilepath.concat("\\zooData.txt");
+        String[] unParsedZooData = new String[0];
+        lineCount = 0;
+        try {
+            File dataFile = new File(zooDataFilepath);
+            Scanner scannerBob = new Scanner(dataFile);
+            while (scannerBob.hasNextLine()){
+                lineCount++;
+                scannerBob.nextLine();
+            }
+            scannerBob.close();
+
+        } catch (FileNotFoundException e){
+            System.out.println("Bad");
+            e.printStackTrace();
+        }
+
+        try {
+            File dataFile = new File(zooDataFilepath);
+            Scanner scannerBob = new Scanner(dataFile);
+            unParsedZooData = new String[lineCount];
+            int i = 0;
+            while (scannerBob.hasNextLine()){
+                unParsedZooData[i] = scannerBob.nextLine();
+                i++;
+            }
+            scannerBob.close();
+
+        } catch (FileNotFoundException e){
+            System.out.println("Bad");
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < lineCount; i++) {
+            addData(unParsedZooData[i], allAnimalData, usedNames, zooIds);
+        }
+
         for (int i = 0; i < 16; i++) {
             addAnimal(unParsedData[i], allAnimalData, allNames, usedNames, zooIds);
         }
 
-        for (String[] t:zooIds){
-            for (String l:t){
-                System.out.println(l);
-            }
-        }
+        updatePopulation(allAnimalData, zooIds);
     }
-    
+
+    public static void updatePopulation(HashMap<String, String[]> allAnimalData, String[][] zoo){
+        String zooDataFilepath = new File("").getAbsolutePath();
+        zooDataFilepath = zooDataFilepath.concat("\\zooData.txt");
+        try {
+            FileWriter writer = new FileWriter(zooDataFilepath);
+            for (int i = 0; i < 4; i++){
+                for (int j = 0; j < 4; j++) {
+                    if (zoo[i][j] != null) {
+                        writer.write(toTextData(allAnimalData.get(zoo[i][j]), zoo[i][j]) + "\n");
+                    }
+                }
+            }
+            writer.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        String zooPopFilepath = new File("").getAbsolutePath();
+        zooPopFilepath = zooPopFilepath.concat("\\zooPopulation.txt");
+        try {
+            FileWriter writer = new FileWriter(zooPopFilepath);
+            writer.write("Hyena Habitat: \n\n");
+            for (int i = 0; i < 4; i++){
+                    if (zoo[0][i] != null) {
+                        writer.write(toTextData(allAnimalData.get(zoo[0][i]), zoo[0][i]) + "\n");
+                }
+            }
+
+            writer.write("\nLion Habitat: \n\n");
+            for (int i = 0; i < 4; i++){
+                if (zoo[1][i] != null) {
+                    writer.write(toTextData(allAnimalData.get(zoo[1][i]), zoo[1][i]) + "\n");
+                }
+            }
+
+            writer.write("\nBear Habitat: \n\n");
+            for (int i = 0; i < 4; i++){
+                if (zoo[2][i] != null) {
+                    writer.write(toTextData(allAnimalData.get(zoo[2][i]), zoo[2][i]) + "\n");
+                }
+            }
+
+            writer.write("\nTiger Habitat: \n\n");
+            for (int i = 0; i < 4; i++){
+                if (zoo[3][i] != null) {
+                    writer.write(toTextData(allAnimalData.get(zoo[2][i]), zoo[3][i]) + "\n");
+                }
+            }
+            writer.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public static String toText(String id, HashMap<String, String[]> allAnimalData){
+        String text;
+
+        String[] data = allAnimalData.get(id);
+        text = id + "; " + data[7] + "; " + data[0] + " years old; birth date " + data[1] + "; " + data[2] + " color; " + data[8] + "; " + data[3] + " pounds; from " + data[4] + ", " + data[5];
+
+        return text;
+    }
+
+    public static String toTextData(String[] data, String id){
+        String text = "";
+        for (String t: data){
+            text = text + t + "; ";
+        }
+        text = text + id;
+        return text;
+    }
+
+    public static void addData(String data, HashMap<String, String[]> allAnimalData, HashMap<String, Boolean> usedNames, String[][] zoo){
+        String[] currentAnimalData = new String[9];
+        String[] splitData = data.split("; ");
+        for (int i = 0; i < 9; i++){
+            currentAnimalData[i] = splitData[i];
+        }
+        allAnimalData.put(splitData[9], currentAnimalData);
+        int speciesId = 0;
+        switch (splitData[9].split("0")[0]) {
+            case "Hy":
+                speciesId = 0;
+                break;
+            case "Li":
+                speciesId = 1;
+                break;
+            case "Be":
+                speciesId = 2;
+                break;
+            case "Ti":
+                speciesId = 3;
+                break;
+            default:
+                speciesId = 0;
+        }
+        zoo[speciesId][Integer.parseInt(splitData[9].split("0")[1])-1] = splitData[9];
+    }
     public static void addAnimal(String[] animalData, HashMap<String, String[]> allAnimalData, String[][] names,HashMap<String, Boolean> usedNames, String[][] zoo){
         String[] currentAnimalData = new String[9];
         currentAnimalData[0] = animalData[0].split(" ")[0];
